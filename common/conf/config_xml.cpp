f26bb03594a9901bcc146715aa4fb3b98bc7a348
dandyhuang <vivo@tx.com>
2022-08-08 11:35:55 +0800

    common

---
+++ common/conf/config_xml.cpp
@@ -0,0 +1,59 @@
+﻿#include "common/conf/config_xml.h"
+
+namespace common {
+
+void ConfigXml::init(const std::string& path, int id_default) {
+  tinyxml2::XMLError err;
+  if (id_default) {
+    err = xml.Parse(path.c_str());
+  } else {
+    err = xml.LoadFile(path.c_str());
+  }
+
+  CHECK(err == tinyxml2::XML_SUCCESS);
+  _node = xml.RootElement();
+}
+
+const tinyxml2::XMLElement* ConfigXml::Node() const { return _node; }
+
+bool ConfigXml::Child(const std::string& name, ConfigXml& out) const {
+  tinyxml2::XMLElement* ret = _node->FirstChildElement(name.c_str());
+  if (ret) {
+    out._node = ret;
+    return true;
+  }
+  return false;
+}
+
+bool ConfigXml::Next(ConfigXml& out) const {
+  tinyxml2::XMLElement* ret = _node->NextSiblingElement();
+  if (ret) {
+    out._node = ret;
+    return true;
+  }
+  return false;
+}
+
+bool ConfigXml::Next(const std::string& name, ConfigXml& out) const {
+  tinyxml2::XMLElement* ret = _node->NextSiblingElement(name.c_str());
+  if (ret) {
+    out._node = ret;
+    return true;
+  }
+  return false;
+}
+
+bool ConfigXml::Find(const std::string& name, ConfigXml& conf) const {
+  if (_node->Name() == name) {
+    conf._node = _node;
+    return true;
+  }
+  return Next(name, conf);
+}
+
+bool ConfigXml::Name(std::string& out) const {
+  out = _node->Name();
+  return true;
+}
+
+}  // namespace vvframe
---
+++ common/conf/config_xml.h
@@ -0,0 +1,52 @@
+﻿
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include "boost/lexical_cast.hpp"
+#include "glog/logging.h"
+#include "tinyxml2.h"
+namespace common {
+
+class ConfigXml {
+ public:
+  explicit ConfigXml() : _node(NULL) {}
+  explicit ConfigXml(const std::string& path) { init(path); }
+  explicit ConfigXml(const ConfigXml& para) { _node = para._node; }
+  explicit ConfigXml(ConfigXml&& para) {
+    _node = para._node;
+    para._node = NULL;
+  }
+
+  void init(const std::string& path, int id_default = 0);
+  const tinyxml2::XMLElement* Node() const;
+  bool Child(const std::string& name, ConfigXml& out) const;
+  template <class T>
+  bool Attr(const std::string& name, T& out) const {
+    const tinyxml2::XMLAttribute* a = _node->FindAttribute(name.c_str());
+    if (a) {
+      out = boost::lexical_cast<T>(a->Value());
+      return true;
+    }
+    return false;
+  }
+  template <class T>
+  bool Attr(const std::string& name, T& out, const T& default_value) const {
+    bool ret = Attr(name, out);
+    if (!ret) {
+      out = default_value;
+    }
+    return ret;
+  }
+
+  bool Next(ConfigXml& out) const;
+  bool Next(const std::string& name, ConfigXml& out) const;
+  bool Find(const std::string& name, ConfigXml& out) const;
+  bool Name(std::string& out) const;
+
+ private:
+  tinyxml2::XMLDocument xml;  //所有_node及其子_node归这个管
+  tinyxml2::XMLElement* _node;
+};
+}  // namespace common
