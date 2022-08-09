0c727148c425d52a4c219a5bdd301de27886d21b
dandyhuang <vivo@tx.com>
2022-08-09 12:52:47 +0800

    update

--- dag_test/dag_01.cpp
+++ dag_test/dag_01.cpp
@@ -7,7 +7,7 @@
 
 namespace dag {
 SERVICE_REGISTER(dag_001)
-int dag_001::do_service(std::shared_ptr<frame::Context> context) {
+int dag_001::do_service(std::shared_ptr<frame::Context> context) noexcept {
    for (auto i = 0; i < 100000; i++) {
       if (i % 1000 == 0) {
          usleep(10);
