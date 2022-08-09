10f20da678aa69003080c9fc7ca8f36befcaf02e
dandyhuang <vivo@tx.com>
2022-08-09 10:02:53 +0800

    update

--- dag/node_manager.h
+++ dag/node_manager.h
@@ -12,7 +12,7 @@ using namespace vlog;
 class NodeManager {
  private:
   NodeManager(){};
-  std::unordered_map<std::string, std::shared_ptr<common::ConfigXml>> nodes_map_;
+  std::unordered_map<std::string, std::shared_ptr<::common::ConfigXml>> nodes_map_;
 
  public:
   static NodeManager& Instance() {
@@ -21,13 +21,13 @@ class NodeManager {
   }
 
   void InitNodeConf(const std::string& file_path);
-  std::shared_ptr<common::ConfigXml> GetNodeInfo(const std::string& node_name) {
+  std::shared_ptr<::common::ConfigXml> GetNodeInfo(const std::string& node_name) {
     auto it = nodes_map_.find(node_name);
     if (it != nodes_map_.end()) {
       return it->second;
     } else {
       std::cout << "node_name: " << node_name << " found in nodes_map_" << std::endl;
-      return std::shared_ptr<common::ConfigXml>();
+      return std::shared_ptr<::common::ConfigXml>();
     }
   }
 };
