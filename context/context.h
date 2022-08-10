8a2b9e75d0d05d7235ad819ab12fbb88024804e2
dandyhuang <vivo@tx.com>
2022-08-10 11:07:52 +0800

    update Dag_Synchronize_Use

--- context/context.h
+++ context/context.h
@@ -6,7 +6,13 @@ namespace frame {
 
 class Context : public dag::GraphContext {
  public:
-  Context(brpc::Controller* cntl, const google::protobuf::Message* input_request, google::protobuf::Message* input_response,
-          google::protobuf::Closure* done) {}
+  Context(brpc::Controller* cntl, const google::protobuf::Message* input_request,
+          google::protobuf::Message* input_response, google::protobuf::Closure* done) {}
+#ifdef Dag_Synchronize_Use
+        std::vector<bthread_t> bt_vec_;
+        std::vector<std::thread> t_vec_;
+#else
+
+#endif
 };
 }  // namespace frame
